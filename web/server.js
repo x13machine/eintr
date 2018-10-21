28bd46d800a35ff9b03f65c0daabc8b52d67c4d6
x13machine <brandanw2@gmail.com>
2018-10-21 03:18:42 -0500

    web public fix

--- web/server.js
+++ web/server.js
@@ -7,7 +7,6 @@ if(process.env.override)config = Object.assign(config,JSON.parse(process.env.ove
 if(process.env.config)config = Object.assign(config,config.override[process.env.config]);
 
 
-console.log(__dirname)
 process.env.NODE_ENV = 'production';
 
 global.htmlToText = require('html-to-text');
@@ -106,7 +105,7 @@ require('./templates.js');
 require('./data.js');
 require('./pages.js');
 require('./ajax.js');
-app.use(express.static(dirname__ + '/public'));
+app.use(express.static(__dirname + '/public'));
 global.server = app.listen(process.env.PORT || config.port || 5555);
 
 process.on('uncaughtException', function(err) {
